fa586c6cd0514d92c40ae94611a201ad28cc2d6d
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2014-06-25 13:37:53 -0400

    Switch to Filer for all fs operations, update to latest API. NOTE: requires patched filer.js https://github.com/humphd/filer/commit/25f2a24cdfe93897ba347e75506160c2acb0eb84

--- src/thirdparty/browser-appshell.js
+++ src/thirdparty/browser-appshell.js
@@ -5,9 +5,7 @@
  *   http://www.hyperlounge.com/blog/hacking-the-brackets-shell/
  */
 
-(function(global, navigator, Filer) {
-
-  var Path = Filer.Path;
+(function(global, navigator) {
 
   var startupTime = Date.now();
   function getElapsedMilliseconds() {
@@ -21,81 +19,6 @@
   // Provide an implementation of appshell.app as expected by src/utils/Global.js
   // and other parts of the code.
   var appshell = global.appshell = global.appshell || {};
-
-  // Brackets expects the appshell to have a filesystem.
-  // TODO: replace this with MakeDrive.
-  appshell.fs = new Filer.FileSystem({
-    provider: new Filer.FileSystem.providers.Fallback()
-  },
-  function(err, fs) {
-    // XXXhack: Pre-populate a few directories/files, so calls into the fs don't totally fail
-    var sh = fs.Shell();
-    sh.mkdirp('/ApplicationSupport/extensions/user');
-
-    // Make brackets happy and create a native brackets dir path, see
-    // https://github.com/adobe/brackets/blob/0f9574cd969639e4564c953e33bbdd0a2f98bb84/src/file/FileUtils.js#L278-L290
-    var pathname = decodeURI(window.location.pathname);
-    var directory = pathname.substr(0, pathname.lastIndexOf("/"));
-
-    sh.mkdirp(Path.join(directory, 'extensions/default'));
-    sh.mkdirp(Path.join(directory, '../samples/root/Getting Started'), function(err) {
-      if(err) return;
-
-      fs.writeFile(Path.join(directory, '../samples/root/Getting Started', 'index.html'), "<p>hello world!</p>");
-    });
-  });
-
-  // The native appshell fs is a bit different from Filer's
-  var fsProto = Filer.FileSystem.prototype;
-  fsProto.ERR_BROWSER_NOT_INSTALLED = 11;
-  fsProto.ERR_CANT_READ = 4;
-  fsProto.ERR_CANT_WRITE = 6;
-  fsProto.ERR_FILE_EXISTS = 10;
-  fsProto.ERR_INVALID_PARAMS = 2;
-  fsProto.ERR_NOT_DIRECTORY = 9;
-  fsProto.ERR_NOT_FILE = 8;
-  fsProto.ERR_NOT_FOUND = 3;
-  fsProto.ERR_OUT_OF_SPACE = 7;
-  fsProto.ERR_UNKNOWN = 1;
-  fsProto.ERR_UNSUPPORTED_ENCODING = 5;
-  fsProto.NO_ERROR = 0;
-
-  fsProto.chmod = function(path, mode, callback) {
-    functionNotImplemented();
-  };
-  fsProto.copyFile = function(src, dest, callback) {
-    functionNotImplemented();
-  };
-  fsProto.isNetworkDrive = function(path, callback) {
-    callback(null, false);
-  };
-  fsProto.makedir = fsProto.mkdir;
-
-  // appshell wants its stats object to have real Date objects
-  fsProto.$stat = fsProto.stat;
-  fsProto.stat = function(path, callback) {
-    this.$stat(path, function(err, stats) {
-      if (err){
-        callback(fsProto.ERR_UNKNOWN);
-        return;
-      }
-
-      stats.mtime = new Date(stats.mtime);
-      stats.ctime = new Date(stats.ctime);
-      stats.atime = new Date(stats.atime);
-
-      callback(null, stats);
-    });
-  };
-  fsProto.moveToTrash = function(path, callback) {
-    functionNotImplemented();
-  };
-  fsProto.showOpenDialog = function (allowMultipleSelection, chooseDirectory, title, initialPath, fileTypes, callback) {
-    functionNotImplemented();
-  };
-  fsProto.showSaveDialog = function (title, initialPath, proposedNewFilename, callback) {
-    functionNotImplemented();
-  };
 
   appshell.app = {
     ERR_NODE_FAILED: -3,
@@ -188,4 +111,4 @@
 
   global.brackets = appshell;
 
-}(window, window.navigator, window.Filer));
+}(window, window.navigator));
