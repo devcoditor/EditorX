ef37f9f89ca8cb3de647e1bb8f15d2b5246ced3f
RayGervais <raygervais@hotmail.ca>
2017-03-06 19:37:18 -0500

    Removed unneeded variables from ConsoleManager.js

--- src/extensions/default/bramble/lib/ConsoleManager.js
+++ src/extensions/default/bramble/lib/ConsoleManager.js
@@ -1,15 +1,7 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var BrambleEvents = brackets.getModule("bramble/BrambleEvents");
-    var EditorManager = brackets.getModule("editor/EditorManager");
-    var CommandManager = brackets.getModule("command/CommandManager");
-    var Commands = brackets.getModule("command/Commands");
-
     var ConsoleManagerRemote = require("text!lib/ConsoleManagerRemote.js");
-    var IFrameBrowser = require("lib/iframe-browser");
-
-    var isConsoleEnabled = false;
 
     function getRemoteScript() {
         return "<script>\n" + ConsoleManagerRemote + "</script>\n";
