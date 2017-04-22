e619a218e43c327c3bd1aac4bf1dd38c73b25c80
Jerry Goguette <jerry_g16@hotmail.com>
2017-04-22 11:54:34 -0400

    Bug 1630 - "Modified BrambleCodeSnippets module to properly detect Editor and Insert Snippets" (#739)
    * Fixed editor selection glitch
    
    * config
    
    * Modified BrambleCodeSnippets module to properly detect Editor
    
    * Fixed Indentation

--- src/extensions/default/bramble/lib/BrambleCodeSnippets.js
+++ src/extensions/default/bramble/lib/BrambleCodeSnippets.js
@@ -6,6 +6,7 @@ define(function (require, exports, module) {
 	
     var CommandManager = brackets.getModule("command/CommandManager");
     var EditorManager  = brackets.getModule("editor/EditorManager");
+    var MainViewManager = brackets.getModule("view/MainViewManager");
 
     var CMD_ADD_CODE_SNIPPET_ID     = "bramble.addCodeSnippet";
 	
@@ -19,7 +20,8 @@ define(function (require, exports, module) {
          * @param {string} snippet The snippet to insert.
          */
         function addCodeSnippet(snippet) {
-            var editor = EditorManager.getFocusedEditor();
+            MainViewManager.focusActivePane();
+            var editor = EditorManager.getActiveEditor();
             if (editor) {
                 var insertionPos = editor.getCursorPos();
                 editor.document.replaceRange(snippet, insertionPos);
